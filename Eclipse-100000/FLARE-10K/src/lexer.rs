8883dcbea58ec5f731bc1b83f3f000ef17c3785a
qclipsing <alkotikotik@gmail.com>
2026-07-15 17:30:49 +0700

    something

--- Eclipse-100000/FLARE-10K/src/lexer.rs
+++ Eclipse-100000/FLARE-10K/src/lexer.rs
@@ -1,8 +1,11 @@
-#[derive(Debug, PartialEq, Clone)]
+use std::iter::Peekable;
+use std::str::Chars;
+
 pub enum Token {
     Func,
     For,
     If,
+    While,
     Inline,
     Outline,
     Def,
@@ -10,11 +13,14 @@ pub enum Token {
     Identifier(String),
     IntLiteral(i32),
     HexLiteral(u32),
-    
+
     TypeU32,
-    TypeI32,
+    TypeU16,
     TypeU8,
+    TypeI32,
     TypeI16,
+    TypeI8,
+    TypeBool,
 
     Equal,
     AddEqual,
@@ -25,13 +31,39 @@ pub enum Token {
     Comma,
     Colon,
     Semicolon,
-    
-    LParen,
+
+    LParen, //()
     RParen,
-    LBracket,
+    LBracket, // []
     RBracket,
-    LBrace,
+    LBrace, //{}
     RBrace,
-    
+
     Comment(String), // For >_ comment
 }
+
+struct Lexer<'lt>{ //lifetime
+    chars: Peekable<Chars<'a>>,
+
+    line: usize,
+    position: usize,
+}
+
+impl<'lt> Lexer<'lt>{
+    fn new(input: &'lt str, line: usize, position: usize) -> Self {
+        Lexer {
+            chars: input.chars().peekable(),
+            line: line,
+            position: position,
+        }
+    }
+
+    //Helpers
+    fn move(&mut self) -> Option<char> {
+        self.input.next();
+    }
+    fn peek(&mut self) -> Option<&char> {
+        self.input.peek()
+    }
+}
+
