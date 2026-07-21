bb07e0d3651e3744cf1cf2413dbbe0d26ad06da5
qclipsing <alkotikotik@gmail.com>
2026-07-21 15:02:04 +0700

    continuing IR

--- Eclipse-100000/FLARE-10K/src/3AC.rs
+++ Eclipse-100000/FLARE-10K/src/3AC.rs
@@ -1,4 +1,6 @@
-//3AC IR generator
+//3AC IR generator, again similar to parser, actually most of compiler parts are very similar
+
+use crate::parser::{Program, FunctionSignature, StructDef, Stmt, Expr, Type};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Insts {
@@ -11,7 +13,6 @@ pub enum Insts {
     Not,
     Shl,
     Shr,
-    Sra,
     Load,
     Ldr,
     Str,
@@ -26,8 +27,9 @@ pub enum Insts {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum IROperand {
-    Constant(i32),
-    Variable(String),
+    SignedConstant(i32),
+    UnsignedConstant(u32),
+    Var(String),
     Temp(usize),
 }
 
@@ -61,6 +63,7 @@ pub enum IRInst {
 pub struct IR {
     instsBuffer: Vec<IRInst>,
     temp_counter: usize,
+    label_couter: usize,
 }
 
 //Helpers
@@ -68,10 +71,77 @@ impl IR {
     pub fn new(){
 
     }
+    pub fn new_temp(&mut self) -> IROperand{
+        let buff = IROperand::Temp(self.temp_counter)
+        self.temp_counter += 1;
+        buff
+    }
+    pub fn reset_temp(&mut self) {
+        self.temp_counter = 0;
+    }
+    pub fn new_label(&mut self, prefix: &str) -> String {
+        let buff = format!("~Lab_{}_{}", prefix, self.label_counter);
+        self.label_counter += 1;
+        buff
+    }
+    pub fn reset_labels(&mut self) {
+        self.label_couter = 0;
+    }
+    pub fn emit(&mut self inst: IRInst) {
+        self.instsBuffer.push(inst);
+    }
 }
 
 impl IR {
-    fn 
+    fn lower_expr(&mut self, expr: &Expr) -> IROperand {
+        match expr {
+            Expr::IntLiteral(a) => IROperand::SignedConstant(*a),
+            Expr::HexLiteral(b) => IROperand::UnsignedConstant(*b),
+            Expr::Identifier(name) => IROperand::Var(name.clone()),
+
+            Expr::Binary {left, op, right} => {
+                let l_op = self.lower_expr(left);
+                let r_op = self.lower_expr(right);
+                let dest = self.new_temp();
+
+                let inst = match op {
+                    BinaryOpKind::Add => IRInst::Add { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::Sub => IRInst::Sub { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::Mul => IRInst::Mul { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::Shl => IRInst::Shl { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::Shr => IRInst::Shr { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::BitwiseAnd => IRInst::And { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::BitwiseOr  => IRInst::Or  { dest: dest.clone(), left: l_op, right: r_op },
+                    BinaryOpKind::BitwiseXor => IRInst::Xor { dest: dest.clone(), left: l_op, right: r_op },
+                    _ => panic!("Division isn't implemented yet");
+                };
+                self.emit(inst);
+                dest
+            }
+
+            Expr::Assign {lhs, rhs} => {
+                let r_op = self.lower_expr(rhs);
+                match lhs.as_ref() {
+                    Expr::Identifier(name) => { //Just assign
+                        self.emit(IRInst::Cpy { dest: IROperand::Variable(name.clone()), src: r_op.clone() });
+                        r_op
+                    }
+                    _ => { // Memory location, deref etc
+                        let ptr_op = self.lower_lvalue(lhs);
+                        self.emit(IRInst::StorePtr { ptr_addr: ptr_op, src: r_op.clone() });
+                        r_op
+                    }
+                }
+            }
+            Expr::Deref(mem) => {
+                let ptr_addr = self.lower_expr(mem);
+                let dest = self.new_temp();
+                self.emit(IRInst::LoadPtr { dest: dest.clone, ptr_addr});
+                dest
+            }
+        }
+
+    }
 }
 
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Eclipse-10000 [WIP]
 This project aims to recreate full computer stack from the very scratch
-[x] 16-bit CPU on logisim using only logic gates 
-[x] 32-bit CPU on system Verilog with Custom ISA 
-[x] Assembler for that CPU 
-[-] Currently working on Compiler than compiles my own programming language to that Assembler
-[.] OS on that programming language
+*[x] 16-bit CPU on logisim using only logic gates
+*[x] 32-bit CPU on system Verilog with Custom ISA
+*[x] Assembler for that CPU
+*[-] Currently working on Compiler than compiles my own programming language to that Assembler
+*[.] OS on that programming language
