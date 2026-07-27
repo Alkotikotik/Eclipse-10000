1e8cb7ab01cf2fad69ca9df859211b071dfade5d
qclipsing <alkotikotik@gmail.com>
2026-07-27 14:29:39 +0700

    continueing codegen

--- Eclipse-100000/FLARE-10K/src/codegen.rs
+++ Eclipse-100000/FLARE-10K/src/codegen.rs
@@ -1,3 +1,6 @@
+use crate::IR3AC::{IRInst}
+
+
 //Codegen, lets see what's it about
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum RegType {
@@ -11,6 +14,21 @@ pub struct Register {
     pub id: u8,
     pub reg_type: RegType,
     pub sub_index: u8,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Location {
+    Register(PhysReg),
+    StackOffset(usize),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct BasicBlock {
+    pub id: usize,
+    pub label: Option<String>,
+    pub body: Vec<IRInst>,
+    pub predecessors: Vec<usize>,
+    pub successors: Vec<usize>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -68,6 +86,53 @@ pub enum AsmInst {
     Jr  (AsmOperand),
     Call (Label),
     Ret,
+
+}
+
+pub struct Codegen<'a> {
+    ir_func: &'a IRFunction,
+    cfg: Vec<BasicBlock>,
+    allocations: HashMap<String, Location>,
+    frame_size: usize,
+    wait_for_the_final_result: Vec<TargetInst>,
+}
+
+impl<'a> Codegen<'a> {
+
+    pub fn new(ir_func: &'a IRFunction) -> Self {
+        let cfg = build_cfg(&ir_func.instructions);
+
+        Self {
+            ir_func,
+            cfg,
+            allocations: HashMap::new(),
+            frame_size: 0,
+            emitted_code: Vec::new(),
+        }
+    }
+
+}
+
+impl<'a> Codegen<'a> {
+
+    pub fn build_bb(body: &IRInst) -> Vec<BasicBlock> { //basic blocks
+        let mut leaders = std::collections::BTreeSet::new(); //Auto sort, plus uniqueness
+        leaders.insert(0);
+
+        for (idx, inst) in body.iter().enumerate() {
+            match inst {
+                IRInst::Label(_) => leaders.insert(idx),
+
+                IRInst::JMP(_) | IRInst::Return(_) | IRInst::AntiEqual{..} | IRInst::Equal{..} | IRInst::AntiLess{..} | IRInst::AntiMore{..} => {
+                    if idx + 1 = body.len() {
+                        leaders.insert(idx + 1);
+                    }
+                }
+                _ => {}
+
+            }
+        }
+    }
 
 }
 
